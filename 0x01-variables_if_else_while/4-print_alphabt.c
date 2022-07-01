1c38d6338bd29b727d90c7ca0009a019c0b50edc
Abdul-Mumin Awinaba <awinabaam@gmail.com>
2022-07-01 06:04:40 -0700

    Add 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,15 @@ int main(void)
         char alphabet;
 
         for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-                if (alphabet != 'q' && alphabet != 'e')
-                        putchar(alphabet);
+	{
+	       	if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
+		}
+	}
+
         putchar('\n');
 
         return (0);
-}       
+}
 
