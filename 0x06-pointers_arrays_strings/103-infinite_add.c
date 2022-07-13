9ff97735d4b5581f98a6776a88063ea9ced3a749
Abdul-Mumin Awinaba <awinabaam@gmail.com>
2022-07-13 12:26:39 -0700

    Modify 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,11 +4,8 @@
  * infinite_add - adds two numbers
  *
  * @n1: first number
- *
  * @n2: second number
- *
  * @r: buffer for result
- *
  * @size_r: buffer size
  *
  * Return: address of r or 0
@@ -16,31 +13,44 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
+	while (n1[a_len] != '\0')
+		a_len++;
+	while (n2[b_len] != '\0')
+		b_len++;
+	if (a_len > b_len)
+		biggest = a_len;
+	else
+		biggest = b_len;
+	if ((biggest + 1) >= size_r)
 		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-			break;
-		m = n / 10;
-		r[k] = n % 10 + '0';
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
-	return (r);
+	r[biggest + 1] = '\0';
+
+	while (biggest >= 0)
+		a = (n1[a_len - 1] - '0');
+		b = (n1[b_len - 1] - '0');
+		if (a_len > 0 && b_len > 0)
+			sum = a + b + carry;
+		else if (a_len < 0 && b_len > 0)
+			sum = b + carry;
+		else if (a_len > 0 && b_len < 0)
+			sum = a + carry;
+		else
+			sum = carry;
+
+		if (sum > 9)
+			carry = sum / 10;
+			sum = (sum % 10) + '0';
+		else
+			carry = 0;
+			sum = sum + '0';
+		r[biggest] = sum;
+		a_len--;
+		b_len--;
+		biggest--;
+	if (*(r) != 0)
+		return (r);
+	else
+		return (r + 1);
 }
