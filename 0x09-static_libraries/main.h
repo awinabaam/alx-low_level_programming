ddde94910882e249cf6408d7f1469eaee37e895d
Abdul-Mumin Awinaba <awinabaam@gmail.com>
2022-07-22 10:36:31 -0700

    Fix errors in libmy.a and main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *
+ * File: main.h
+ * Author: Abdul-Mumin Awinaba
+ * Description: header file containing all the function prototypes in 
+ * 		the static library libmy.a
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +29,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
